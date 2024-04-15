6aaf31997d9493fa9e178306e9c720db632d13e9
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-04-15 15:12:12 +0200

    Update 12_HaplotypeCalling.sh

--- 12_HaplotypeCalling.sh
+++ 12_HaplotypeCalling.sh
@@ -11,17 +11,14 @@ module purge
 module load GATK/4.2.5.0-GCCcore-11.2.0-Java-11
 
 path_list_fastq="/scratch_isilon/groups/compgen/easensi/Captive_TFM/samples_for_vcf" #path to the file with the ids for samples that undergo haplotye calling
-chrom="/scratch_isilon/groups/compgen/easensi/Captive_TFM/chromosomes" #path to the file with the chromosome identifiers
+path_list_chrom="/scratch_isilon/groups/compgen/easensi/Captive_TFM/chromosomes" #path to the file with the chromosome identifiers
 bam="/scratch_isilon/groups/compgen/easensi/Captive_TFM/Filter" #path to the bam files folder 
 ref="/scratch/devel/malvarest/refs/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna" #path to the reference genome
 vcf="/scratch_isilon/groups/compgen/easensi/Captive_TFM/HaplotypeCalling"; mkdir -p $vcf #path and creation of the folder where the VCF will be saved
+samples=$(awk -v line="$SLURM_ARRAY_TASK_ID" 'NR==line {print $0}' ${path_list_fastq}) #extraction of the sample name
 
-for SLURM_ARRAY_TASK_ID in $(seq $SLURM_ARRAY_TASK_MIN $SLURM_ARRAY_TASK_MAX); do
-fastq=$(awk -v line="$SLURM_ARRAY_TASK_ID" 'NR==line {print $0}' ${path_list_fastq}) #extraction of the sample name
-
-while IFS= read -r i; do #construct -L option for all the genomic intervals for each sample
-intervals+=" --L ${i}"
-done < "${chrom}"
-
-gatk HaplotypeCaller -I ${bam}/${fastq}_filtered_2.bam -O ${vcf}/${fastq}.vcf.gz -R $ref ${intervals} --ERC BP_RESOLUTION
+for i in "${samples[@]}"; do
+  while IFS= read -r chrom; do #Loop for each chromosome
+  gatk HaplotypeCaller -I ${bam}/${i}_filtered_2.bam -O ${vcf}/${i}_${chrom}.vcf.gz -R $ref --L ${chrom} --ERC BP_RESOLUTION
+  done < "${path_list_chrom}"
 done
