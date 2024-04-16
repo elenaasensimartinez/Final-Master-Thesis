422fbb669f3de9e45db9191c73416d8c909625c1
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-04-16 10:13:58 +0200

    Update 12_HaplotypeCalling.sh

--- 12_HaplotypeCalling.sh
+++ 12_HaplotypeCalling.sh
@@ -2,10 +2,10 @@
 #SBATCH -J HapCall #job name
 #SBATCH -q long #maximum limits available for the job
 #SBATCH -c 4 #cpu per task
-#SBATCH --mem 100G #memory pool for all cores MB
-#SBATCH -o HapCall_%a.out #STDOUT
-#SBATCH -e HapCall_%a.err #STDERR
-#SBATCH -a 1-2 #number of samples
+#SBATCH --mem 150G #memory pool for all cores MB
+#SBATCH -o HapCall_def_%a.out #STDOUT
+#SBATCH -e HapCall_def_%a.err #STDERR
+#SBATCH -a 1-24 #number of samples
 
 module purge
 module load GATK/4.2.5.0-GCCcore-11.2.0-Java-11
@@ -14,11 +14,11 @@ path_list_fastq="/scratch_isilon/groups/compgen/easensi/Captive_TFM/samples_for_
 path_list_chrom="/scratch_isilon/groups/compgen/easensi/Captive_TFM/chromosomes" #path to the file with the chromosome identifiers
 bam="/scratch_isilon/groups/compgen/easensi/Captive_TFM/Filter" #path to the bam files folder 
 ref="/scratch/devel/malvarest/refs/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna" #path to the reference genome
-vcf="/scratch_isilon/groups/compgen/easensi/Captive_TFM/HaplotypeCalling"; mkdir -p $vcf #path and creation of the folder where the VCF will be saved
-samples=$(awk -v line="$SLURM_ARRAY_TASK_ID" 'NR==line {print $0}' ${path_list_fastq}) #extraction of the sample name
+vcf="/scratch_isilon/groups/compgen/easensi/Captive_TFM/HaplotypeCalling_2"; mkdir -p $vcf #path and creation of the folder where the VCF will be saved
+samples=$(awk -v line="$SLURM_ARRAY_TASK_ID" 'NR==line {print $0}' ${path_list_chrom}) #extraction of the chromosome name
 
 for i in "${samples[@]}"; do
-  while IFS= read -r chrom; do #Loop for each chromosome
-  gatk HaplotypeCaller -I ${bam}/${i}_filtered_2.bam -O ${vcf}/${i}_${chrom}.vcf.gz -R $ref --L ${chrom} --ERC BP_RESOLUTION
-  done < "${path_list_chrom}"
+  while IFS= read -r fastq; do #Loop for each fastq
+  gatk HaplotypeCaller -I ${bam}/${fastq}_filtered_2.bam -O ${vcf}/${fastq}_${i}.vcf.gz -R $ref --L ${i} --ERC BP_RESOLUTION
+  done < "${path_list_fastq}"
 done
