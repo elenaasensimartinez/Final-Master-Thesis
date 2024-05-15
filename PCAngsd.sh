8c29d0e5a6598afe78276a2faa49050ee574e893
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-05-15 12:36:51 +0200

    Create PCAngsd.sh

---
+++ PCAngsd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J PCA #job name
+#SBATCH -q short #maximum limits available for the job
+#SBATCH -c 2 #cpu per task
+#SBATCH --mem 50G #memory pool for all cores MB
+#SBATCH -o PCA.out #STDOUT
+#SBATCH -e PCA.err #STDERR
+
+inputbeaglegz="/scratch_isilon/groups/compgen/easensi/Captive_TFM/Downsampling_bams/merged_downsampled_output.beagle.gz"
+OUT="/scratch_isilon/groups/compgen/easensi/Captive_TFM/PCA_all"; mkdir -p $OUT
+pcangsd="${OUT}/pcangsd"
+
+module purge
+module load angsd/0.935-GCC-10.2.0
+module load Python/3.10.8-GCCcore-12.2.0
+pip3 install --user -r ~jprado/bin/pcangsd_folder/requirements.txt
+~jprado/bin/pcangsd -b ${inputbeaglegz} -o ${pcangsd}
