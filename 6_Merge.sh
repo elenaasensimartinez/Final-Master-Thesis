861b64a1c4079c30476d43fd512eb0203be062be
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-06-28 12:35:42 +0200

    Update 6_Merge.sh

--- 6_Merge.sh
+++ 6_Merge.sh
@@ -5,15 +5,15 @@
 #SBATCH --mem 100G #memory pool for all cores MB
 #SBATCH -o Mergebam_%a.out #STDOUT
 #SBATCH -e Mergebam_%a.err #STDERR
-#SBATCH -a 1-3 #number of samples
+#SBATCH -a 1-3 #array number
 
 module purge
 module load SAMtools/1.15-GCC-11.2.0
 
-path_list_fastq="/scratch_isilon/groups/compgen/easensi/Captive_TFM/FilterInsertSize_2/filtered_names"
+path_list_fastq="/scratch_isilon/groups/compgen/easensi/Captive_TFM/FilterInsertSize_2/filtered_names" #path to fastqs
 
 fastq=$(awk -v line="$SLURM_ARRAY_TASK_ID" 'NR==line {print $0}' ${path_list_fastq})
-OUT="/scratch_isilon/groups/compgen/easensi/Captive_TFM/MergedBam"; mkdir -p $OUT
-bamlist="/scratch_isilon/groups/compgen/easensi/Captive_TFM/FilterInsertSize_2/"
+OUT="/scratch_isilon/groups/compgen/easensi/Captive_TFM/MergedBam"; mkdir -p $OUT #path to saving folder
+bamlist="/scratch_isilon/groups/compgen/easensi/Captive_TFM/FilterInsertSize_2/" #path to BAM files list
 
-samtools merge - ${bamlist}*${fastq}_filtered.bam | samtools view -hb > ${OUT}/${fastq}_merged.bam
+samtools merge - ${bamlist}*${fastq}_filtered.bam | samtools view -hb > ${OUT}/${fastq}_merged.bam #merging BAM files according to sample
