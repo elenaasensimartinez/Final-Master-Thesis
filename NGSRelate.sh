49d349022f746ed30db8d3cb465c461b87c9891c
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-06-28 14:07:28 +0200

    Update NGSRelate.sh

--- NGSRelate.sh
+++ NGSRelate.sh
@@ -7,4 +7,5 @@ module purge
 module load OpenSSL
 module load Python/3.10.8-GCCcore-12.2.0
 
-/home/groups/compgen/jprado/bin/ngsRelate/ngsRelate -g merged_western_lowland_relate.gz -n 187 -f freq_merged -O marathon_ngs_western_lowland
+                                                                      ### NGSrelate analysis ###
+/home/groups/compgen/easensi/bin/ngsRelate/ngsRelate -g merged_western_lowland_relate.gz -n 187 -f freq_merged -O marathon_ngs_western_lowland
