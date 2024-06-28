042487fcde94a32e389548fc00aa84cb23430020
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-06-28 14:00:34 +0200

    Update 17_Concat_Filtered_vcfs.sh

--- 17_Concat_Filtered_vcfs.sh
+++ 17_Concat_Filtered_vcfs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH -J concat #job name
-#SBATCH -q vlong
-#SBATCH -c 4
-#SBATCH --mem-per-cpu 40G
+#SBATCH -q vlong #running time
+#SBATCH -c 4 #number of CPUs
+#SBATCH --mem-per-cpu 40G #memory per CPU
 
 
 module purge
 module load BCFtools/1.17-GCC-12.2.0
 
+    ### concat all VCFs for each chromosome into one ###
 bcftools concat -f vcf_file.txt -Oz  > merged_vcf_filter.vcf.gz
