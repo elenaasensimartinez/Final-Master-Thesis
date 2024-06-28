ffd6cb7c6c8c60400b030fddebbf267dfa3e6d40
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-06-28 12:46:39 +0200

    Update and rename 13.2.1_MergeAngsd.sh to 13.2.1_MergeAngsd_for_NGSRelate.sh

--- 13.2.1_MergeAngsd_for_NGSRelate.sh
+++ 13.2.1_MergeAngsd_for_NGSRelate.sh
@@ -6,8 +6,10 @@
 #SBATCH -o MergeAngsd.out #STDOUT
 #SBATCH -e MergeAngsd.err #STDERR
 
-chromfile="/scratch_isilon/groups/compgen/easensi/NGS_relate/western_lowland"
-out="/scratch_isilon/groups/compgen/easensi/NGS_relate/western_lowland/merged_western_lowland_relate.gz"
+chromfile="/scratch_isilon/groups/compgen/easensi/NGS_relate/western_lowland" #path to file with chromosome names
+out="/scratch_isilon/groups/compgen/easensi/NGS_relate/western_lowland/merged_western_lowland_relate.gz" #path to saving folder
+
+###first a file with chr1 information must be saved in merged_western_lowland_relate.gz ###
 for CHR in `seq 2 24`
 do
   gunzip -c ${chromfile}/chr${CHR}_western_lowland_genolike.glf.gz | gzip -c
