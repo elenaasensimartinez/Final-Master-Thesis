c925be77e37bbfb4619298fc6b907c74d034429c
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-06-28 12:27:55 +0200

    Update 2_FastP.sh

--- 2_FastP.sh
+++ 2_FastP.sh
@@ -5,17 +5,18 @@
 #SBATCH --mem 50G #memory pool for all cores MB
 #SBATCH -o FASTP_%a.out #STDOUT
 #SBATCH -e FASTP_%a.err #STDERR
-#SBATCH -a 1-44 #number of samples
+#SBATCH -a 1-44 #array number
 
 module purge
 module load fastp/0.23.2-GCC-11.3.0 
-path_list_fastq="/scratch_isilon/groups/compgen/iruiz/EEP_gorillas/FTAcards_Hairs/FASTQ/CGLILLUMINA_34/fastqs"
+
+path_list_fastq="/scratch_isilon/groups/compgen/iruiz/EEP_gorillas/FTAcards_Hairs/FASTQ/CGLILLUMINA_34/fastqs" #path to fastqs
 fastq=$(awk -v line=“$SLURM_ARRAY_TASK_ID” ‘NR==line {print $0}’ ${path_list_fastq})
 
 DIR=“scratch_isilon/groups/compgen/iruiz/EEP_gorillas/FTAcards_Hairs/FASTQ/”
 IN=${DIR}”CGLILLUMINA_34/”
 
-OUT=“scratch_isilon/groups/compgen/easensi/Captive_TFM/FastP_trim/”; mkdir -p $OUT
+OUT=“scratch_isilon/groups/compgen/easensi/Captive_TFM/FastP_trim/”; mkdir -p $OUT #path to saving folder
 p1=${IN}${fastq}_1.fastq.gz
 p2=${IN}${fastq}_2.fastq.gz
 out_1=${OUT}${fastq}_trimmed_1.fastq.gz
@@ -25,4 +26,4 @@ json=${OUT}${fastq}.fastp.json
 fastp -i ${p1} -I ${p2} --out1 ${out_1} --out2 ${out_2} --detect_adapter_for_pe -p --
 adapter_sequence=AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --
 adapter_sequence_r2=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT --trim_poly_g --poly_g_min_len 10 --
-length_required 30 --json ${json} --html ${OUT}${fastq}.html
+length_required 30 --json ${json} --html ${OUT}${fastq}.html #trimming procedure with ILLUMINA adapters
