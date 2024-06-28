5b785b57ff18d1bcb8fce7cab4759a28d273eae2
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-06-28 12:51:06 +0200

    Update 14_CombineVCFs.sh

--- 14_CombineVCFs.sh
+++ 14_CombineVCFs.sh
@@ -5,16 +5,16 @@
 #SBATCH --mem 150G #memory pool for all cores MB
 #SBATCH -o MergeGVCFs_sec_%a.out #STDOUT
 #SBATCH -e MergeGVCFs_sec_%a.err #STDERR
-#SBATCH -a 1-24
+#SBATCH -a 1-24 #array number
 
 module purge
 module load GATK/4.2.5.0-GCCcore-11.2.0-Java-11
 
-OUT="/scratch_isilon/groups/compgen/easensi/Captive_TFM/MergeGVCFs_sec"; mkdir -p $OUT
-ref="/scratch/devel/malvarest/refs/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+OUT="/scratch_isilon/groups/compgen/easensi/Captive_TFM/MergeGVCFs_sec"; mkdir -p $OUT #path to saving folder
+ref="/scratch/devel/malvarest/refs/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna" #path to reference genome
 path_list_list="/scratch_isilon/groups/compgen/easensi/Captive_TFM/Script14_combine"
 path_list_chrom="/scratch_isilon/groups/compgen/easensi/Captive_TFM/Script14_combine/chromfile"
-samples=$(awk -v line="$SLURM_ARRAY_TASK_ID" 'NR==line {print $0}' ${path_list_chrom}) #extraction of the chromosome name
+samples=$(awk -v line="$SLURM_ARRAY_TASK_ID" 'NR==line {print $0}' ${path_list_chrom}) 
 
-
+                                         ### combine haplotype calling files ###
  gatk CombineGVCFs  -R ${ref}  --variant ${path_list_list}/List_${samples}.list -O ${OUT}/${samples}_sec_combined.vcf.gz
