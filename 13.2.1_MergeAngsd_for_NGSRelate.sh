59b140d0f06c0f82c7e72ba771664ef75fd15ecc
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-06-28 12:47:26 +0200

    Update 13.2.1_MergeAngsd_for_NGSRelate.sh

--- 13.2.1_MergeAngsd_for_NGSRelate.sh
+++ 13.2.1_MergeAngsd_for_NGSRelate.sh
@@ -9,7 +9,7 @@
 chromfile="/scratch_isilon/groups/compgen/easensi/NGS_relate/western_lowland" #path to file with chromosome names
 out="/scratch_isilon/groups/compgen/easensi/NGS_relate/western_lowland/merged_western_lowland_relate.gz" #path to saving folder
 
-###before running this script, a file with chr1 information must be saved in merged_western_lowland_relate.gz ###
+### before running this script, a file with chr1 information must be saved in merged_western_lowland_relate.gz ###
 ### obtention of a merged file with all beagle files of all chromosomes ###
 for CHR in `seq 2 24`
 do
