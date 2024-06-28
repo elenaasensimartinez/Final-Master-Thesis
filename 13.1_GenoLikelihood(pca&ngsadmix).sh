74f63a5ed5426960ed4f281b1bc90d38c2f8af4f
elenaasensimartinez <166503496+elenaasensimartinez@users.noreply.github.com>
2024-06-28 12:44:24 +0200

    Update 13.1_GenoLikelihood(pca&ngsadmix).sh

--- 13.1_GenoLikelihood(pca&ngsadmix).sh
+++ 13.1_GenoLikelihood(pca&ngsadmix).sh
@@ -11,9 +11,10 @@ module purge
 module load OpenSSL
 module load angsd/0.935-GCC-10.2.0
 
-bamfilelist="/scratch_isilon/groups/compgen/easensi/Captive_TFM/Listfile" #path to the bam files folder 
-OUT="/scratch_isilon/groups/compgen/easensi/Captive_TFM/Geno_likelihood/"; mkdir -p $OUT
+bamfilelist="/scratch_isilon/groups/compgen/easensi/Captive_TFM/Listfile" #path to the BAM files folder 
+OUT="/scratch_isilon/groups/compgen/easensi/Captive_TFM/Geno_likelihood/"; mkdir -p $OUT #path to saving folder
 genolike="${OUT}final_captive.genolike"
-ref="/scratch/devel/malvarest/refs/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+ref="/scratch/devel/malvarest/refs/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna" #path to reference genome
 
+                        ### obtention of a beagle file per chromosome with all samples ###
 angsd -GL 2 -out ${genolike} -nThreads 10 -doGlf 2 -doMajorMinor 1 -doMaf 1 -r chr21: -SNP_pval 1e-6 -bam "${bamfilelist}" -ref ${ref} -trim 0 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -C 50 -baq 1 -skipTriallelic 1 -minMapQ 25 -minQ 20 -minMaf 0.004 #depends on the number of samples and the population
